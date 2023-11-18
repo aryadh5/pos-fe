651c4204c5ebba4bb559d3a6f66ccc2d029d6bbf
arya difa <aryadifah@gmail.com>
2023-11-18 16:27:05 +0700

    Initial commit

--- app/src/main/java/com/example/tesfe/activities/MainActivity.java
+++ app/src/main/java/com/example/tesfe/activities/MainActivity.java
@@ -1,30 +1,17 @@
 package com.example.tesfe.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.example.tesfe.R;
-
+import android.view.Menu;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.tesfe.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button navigateButton = findViewById(R.id.button2);
-        navigateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HelloActivity.class);
-                startActivity(intent);
-            }
-        });
-
-    }
 }
-
